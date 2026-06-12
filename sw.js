a2be1eb433c405b1f17053e70948a58cb2ad3a2c
dairojnl300-collab <dairoj.nl300@gmail.com>
2026-06-11 21:00:26 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,22 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys()
-            .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-            .then(() => self.clients.claim())
-            .then(() => self.registration.unregister())
-    );
+const CACHE_NAME = 'cardique-v1';
+const ASSETS = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
 });
