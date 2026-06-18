aeba2e55b28bda7f9542971f17965f2ac1ef75e6
Dairo Narvaez <dairoj.nl300@gmail.com>
2026-06-17 22:21:11 -0500

    fix: usar resClone antes de consumir body en fetch handler

--- sw.js
+++ sw.js
@@ -19,7 +19,8 @@ self.addEventListener('fetch', e => {
   if (e.request.mode === 'navigate' || e.request.url.includes('index.html')) {
     e.respondWith(
       fetch(e.request).then(res => {
-        caches.open(CACHE_NAME).then(cache => cache.put(e.request, res.clone()));
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, resClone));
         return res;
       }).catch(() => caches.match(e.request))
     );
