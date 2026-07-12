4af95ca8af80d6fc3d278e2f6b85bb429b262bc6
Dairo Narvaez <dairoj.nl300@gmail.com>
2026-07-12 18:58:11 -0500

    feat: comparacion semanal con N frentes simultaneos
    Permite elegir cuantos periodos comparar (2 a N) con selectores dinamicos, graficos y PDF multi-periodo, manteniendo el auto-match de 2 inspecciones por defecto.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// build: 2026-07-12-foto-items-filtro-areas
-const APP_VERSION = '1.5.88';
+// build: 2026-07-12-comparacion-semanal-n-frentes
+const APP_VERSION = '1.5.89';
 const CACHE_NAME = 'cardique-v' + APP_VERSION;
 
 self.addEventListener('install', event => {
