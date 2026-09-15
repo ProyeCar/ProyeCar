a3625c4e7143471b856bc6c9a9763a02fcdb502a
Dairo Narvaez <dairoj.nl300@gmail.com>
2026-09-14 21:05:11 -0500

    fix: guard dashboard PDF capture dimensions

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // build: 2026-08-31-editar-nombre-frente-historial
-const APP_VERSION = '1.5.186';
+const APP_VERSION = '1.5.187';
 const CACHE_NAME = 'cardique-v' + APP_VERSION;
 const VERSION_CHECK_INTERVAL_MS = 5 * 60 * 1000;
 
